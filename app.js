bdca5a821af9e4acd4a5e9f97f1ce081c44dd7de
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-09-05 15:02:46 -0300

    fetch de produto pelo click , inicio do modal produto

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
+    produto: {},
   },
   methods: {
     getProdutos() {
       fetch("./api/produtos.json").then((res) => {
-        console.log(res);
         res.json().then((json) => {
+          // console.log(json);
           this.produtos = json;
+        });
+      });
+    },
+    getProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`).then((res) => {
+        res.json().then((json) => {
+          console.log(json);
+          this.produto = json;
         });
       });
     },
   },
   filters: {
-    precoProduto(valor) {
+    precoItem(valor) {
       return valor.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL",
@@ -23,6 +32,5 @@ const vm = new Vue({
   },
   created() {
     this.getProdutos();
-    console.log(this.produtos);
   },
 });
